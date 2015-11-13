dc4d3205f0760b2fe179dba081713306e7c97d61
Dustin Noyes <dustin.noyes@gmail.com>
2015-11-12 16:47:04 -0800

    simplified BuildSenators and added rollcall grabbin

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var BuildSenators = require(__dirname + '/lib/BuildSenators.js');
+var BuildRollCallVote = require(__dirname + '/lib/BuildRollCallVote');
 
 
 
 BuildSenators();
+//BuildRollCallVote();
