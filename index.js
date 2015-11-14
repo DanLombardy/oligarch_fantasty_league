b2fc83bf63a4d957babaa209641d481733c246b6
Dustin Noyes <dustin.noyes@gmail.com>
2015-11-14 11:47:19 -0800

    updated package.json

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var BuildSenators = require(__dirname + '/lib/BuildSenators.js');
 var BuildRollCallVote = require(__dirname + '/lib/BuildRollCallVote');
 var vote2Voter = require(__dirname + '/lib/matchSenatorsVotes');
+var Promise = require('promise');
+
+
+  function doSomething() {
+    return {
+      then: function getVoters(callback){
+        var x = vote2Voter('S001141');
+        console.log(x);
+        callback(x);
+      }
+    };
+  }
+  doSomething().then(function(result) {
+      console.log('my result' + result)
+  });
 
 
 
-//BuildSenators();
-BuildRollCallVote(110, 'senate', 2, 159, vote2Voter);
