8259eb6439c94b347d7e290097d9aebed28bfe0b
Dustin Noyes <dustin.noyes@gmail.com>
2015-11-14 22:46:56 -0800

    added functions to get FEC ids

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var BuildSenators = require(__dirname + '/lib/BuildSenators.js');
 var BuildRollCallVote = require(__dirname + '/lib/BuildRollCallVote');
+var BuildFinancials = require(__dirname + '/lib/buildfinancialdetails');
 var vote2Voter = require(__dirname + '/lib/matchSenatorsVotes');
 var Promise = require('promise');
+var populateFECs = require((__dirname + '/lib/populateFECs'))
 
 
-//this function will pull a senators 5 most recent votes from the db
+
+
+
+//BuildSenators();//
+//BuildFinancials(2016, 'P60006723');
+
+//DON'T DELETE:  this function will pull a senators 5 most recent votes from the db
+/*
 vote2Voter.myRecentVotes('B000944')
 .then(function (result) {
     console.log(result);
@@ -12,5 +21,7 @@ vote2Voter.myRecentVotes('B000944')
 .fail(function (error) {
     console.log(error)
 });
+*/
+
 
 
