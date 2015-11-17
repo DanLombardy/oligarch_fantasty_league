e1389736049eead9b45d18e13641a9464211f319
DanLombardy <lombardy.dan@gmail.com>
2015-11-16 16:11:35 -0800

    Add basic frame for server and initial react demo

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-var BuildSenators = require(__dirname + '/lib/BuildSenators.js');
+/*var BuildSenators = require(__dirname + '/lib/BuildSenators.js');
 var BuildRollCallVote = require(__dirname + '/lib/BuildRollCallVote');
 var BuildFinancials = require(__dirname + '/lib/buildfinancialdetails');
 var vote2Voter = require(__dirname + '/lib/matchSenatorsVotes');
 var Promise = require('promise');
 var populateFECs = require((__dirname + '/lib/populateFECs'))
+var BuildCommitteeData = require(__dirname + '/lib/buildcommitteedata');
+var BuildCommitteeContributes = require(__dirname + '/lib/buildcommcontribs');
+*/
+var http = require('http');
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
+//BuildCommitteeContributes(2012, 'C00495861');
+
+var server = app.listen(4000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("example app listening at http://%s:%s", host, port);
+});
+
+
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/', function (req, res) {
+  //res.send(fs.readFileSync(__dirname + '/public/senator_compare.html'));
+  fs.readFile(__dirname + "/public/senator_compare.html", function(err, data){
+        var parsed = data.toString();
+        res.send(parsed);
+  });
+
+
+});
 
 
 
@@ -22,6 +51,3 @@ vote2Voter.myRecentVotes('B000944')
     console.log(error)
 });
 */
-
-
-
